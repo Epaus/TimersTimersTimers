e5a898e4d089fc29a9b1495cdd73f4e1a6720422
epaus <epaus@paushaus.net>
2020-07-02 15:26:59 -0700

    Initial Commit

---
+++ TimersTimersTimers WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  TimersTimersTimers WatchKit Extension
+//
+//  Created by Estelle Paus on 7/2/20.
+//  Copyright © 2020 Paus Productions. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
